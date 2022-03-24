0e4e95e57675cc8e2086fcb3e569c11d3d1aa68e
wangtao2001 <wangtao.cpu@gmail.com>
2022-03-25 01:43:52 +0800

    添加了导航栏以及热搜页头部

---
+++ WeiboEts/entry/src/ohosTest/java/info/shuangchen/weiboets/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package info.shuangchen.weiboets;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("info.shuangchen.weiboets", actualBundleName);
+    }
+}
